d4a8f680a463367ba3d17f01d6c8329d71b25b9d
Joezeo <1252795700@qq.com>
2019-11-30 14:46:31 +0800

    add sort algorithm:bubble,add util class:ArrayUtils.

--- src/com/joezeo/algorithm/bubblesort/BubbleSort.java
+++ src/com/joezeo/algorithm/bubblesort/BubbleSort.java
@@ -11,18 +11,18 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         //测试
-        int[] arr = {21,2,99210,32,44,1123,4,0,123,-332,-42,112,123,9981,1023,6604,1,-9};
+        int[] arr = {21, 2, 99210, 32, 44, 1123, 4, 0, 123, -332, -42, 112, 123, 9981, 1023, 6604, 1, -9};
         bubbleSort(arr);
         //输出结果：[-332, -42, -9, 0, 1, 2, 4, 21, 32, 44, 112, 123, 123, 1023, 1123, 6604, 9981, 99210]
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int[] arr){
-        for(int i=0; i<arr.length-1; i++){
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             //冒泡算法每一次内外循环后可以确定一个最高位
-            for(int j=0; j<arr.length-1-i; j++){
-                if(arr[j] > arr[j+1]){//升序排序，修改符号可改变升降序方式
-                    ArrayUtils.swap(arr, j, j+1);
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {//升序排序，修改符号可改变升降序方式
+                    ArrayUtils.swap(arr, j, j + 1);
                 }
             }
         }
--- src/com/joezeo/utils/ArrayUtils.java
+++ src/com/joezeo/utils/ArrayUtils.java
@@ -1,15 +1,21 @@
 package com.joezeo.utils;
 
+/**
+ * 数组相关工具类
+ */
 public class ArrayUtils {
-    private ArrayUtils(){};
+    private ArrayUtils() {
+    }
+
 
     /**
      * 数组arr，i、j下标交换数值
+     *
      * @param arr
      * @param i
      * @param j
      */
-    public static void swap(int[] arr, int i, int j){
+    public static void swap(int[] arr, int i, int j) {
         //一个数^（亦或）同一个数两次，结果不变
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j]; //此时arr[j] = arr[i] ^ arr[j] ^ arr[j] = arr[i]
